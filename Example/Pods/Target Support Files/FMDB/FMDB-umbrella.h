8f758716741deed7d8a87cda608ad63d7073c7b5
David Rees <david@appfactory.com.au>
2016-10-19 16:16:32 +1000

    remove pod files

--- Example/Pods/Target Support Files/FMDB/FMDB-umbrella.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "FMDatabase.h"
-#import "FMDatabaseAdditions.h"
-#import "FMResultSet.h"
-
-FOUNDATION_EXPORT double FMDBVersionNumber;
-FOUNDATION_EXPORT const unsigned char FMDBVersionString[];
-
